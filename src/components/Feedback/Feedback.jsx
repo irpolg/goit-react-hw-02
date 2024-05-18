20eac5596bba97b50eb07a05f8c6a87f8c30b5f0
Iryna P <irpolg@ukr.net>
2024-05-18 19:42:27 +0300

    2hw-react-4no-butt

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,24 +1,24 @@
 // import Notification from '../Notification/Notification';
-// import clsx from 'clsx';
-// import css from './Feedback.module.css';
+import clsx from 'clsx';
+import css from './Feedback.module.css';
 
-// export default function Feedback({good, bad, neutral, total, positive, noFeedback,}) {
-//   return noFeedback ? (<Notification />) : (
-//       <div className={css.feedbackList}>
-//       <>
-//       <p className={css.feedbackItem}>Good: {good}</p>
-//       <p className={css.feedbackItem}>Neutral: {neutral} </p>
-//       <p className={css.feedbackItem}>Bad: {bad}</p>
-//       <p className={css.feedbackItem}>Total: {total}</p>
-//       <p
-//         className={clsx(
-//           css.feedbackItem,
-//           positive >= 50 ? css.yesPositive : css.noPositive
-//         )}
-//       >
-//         Positive: {positive}%
-//       </p>
-//     </>
-//     </div>
-//   );
-// }
+export default function Feedback({good, bad, neutral, total, positive}) {
+  return (
+      <div className={css.feedbackList}>
+      <>
+      <p className={css.feedbackItem}>Good: {good}</p>
+      <p className={css.feedbackItem}>Neutral: {neutral} </p>
+      <p className={css.feedbackItem}>Bad: {bad}</p>
+      <p className={css.feedbackItem}>Total: {total}</p>
+      <p
+        className={clsx(
+          css.feedbackItem,
+          positive >= 50 ? css.yesPositive : css.noPositive
+        )}
+      >
+        Positive: {positive}%
+      </p>
+    </>
+    </div>
+  );
+}
