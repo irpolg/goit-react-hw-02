c7d38ba767af083e1ccb98c67e18e079465a7f7b
Iryna P <irpolg@ukr.net>
2024-05-18 21:26:09 +0300

    2hw-react-5-nobutt

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,7 @@ import Description from '../Description/Description';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
 import Notification from '../Notification/Notification';
-
 import './App.css';
-
 import { useState, useEffect } from 'react';
 
 const initFeedback = () => {
@@ -18,66 +16,57 @@ const initFeedback = () => {
       };
 };
 
-// створює стан feedback та функцію setFeedback, початкове значення =initFeedback
 function App() {
-  const [feedback, setFeedback] = useState(initFeedback);
+    const [feedback, setFeedback] = useState(initFeedback);
 
-// збереження відгуку в локальному сховищ
-  useEffect(() => {
-    localStorage.setItem('current-feedback', JSON.stringify(feedback));
-  }, [feedback]);
+    useEffect(() => {
+        localStorage.setItem('current-feedback', JSON.stringify(feedback));
+    }, [feedback]);
   
 //   ЗГІДНО УМОВИ updateFeedback 
-//   const updateFeedback = feedbackType => {
-//     setFeedback({
-//       ...feedback,
-//       [feedbackType]: feedback[feedbackType] + 1,
-//     });
-    //   };
     const updateFeedback = feedbackType => {
-    setFeedback(prevFeedback => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
-  };
+        setFeedback(prevFeedback => ({
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+        }));
+    };
+    
+    const resetFeedback = () => {
+        setFeedback({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+        });
+    };
 
-  const resetFeedback = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-
-  const goodFeedback = feedback.good;
-  const neutralFeedback = feedback.neutral;
-  const badFeedback = feedback.bad;
-
-// ЗГІДНО УМОВИ totalFeedback
-  const totalFeedback = goodFeedback + neutralFeedback + badFeedback;
-  const positiveFeedback = Math.round(
-    ((goodFeedback) / totalFeedback) * 100);
-  const nullFeedback = totalFeedback === 0;
-
-  return (
+    const { good, neutral, bad } = feedback;
+//  ЗГІДНО УМОВИ totalFeedback
+    const totalFeedback = good + neutral + bad;  
+    const positiveFeedback = Math.round(
+        (good / totalFeedback) * 100);
+    
+    const nullFeedback = totalFeedback === 0;
+    
+    return (
     <>
-      <Description />
+        <Description />
+            
+        <Options onClick={() => updateFeedback('good')}>Good</Options>
+        <Options onClick={() => updateFeedback('neutral')}>Neutral</Options>
+        <Options onClick={() => updateFeedback('bad')}>Bad</Options>
+        {!nullFeedback && <Options onBtnClick={resetFeedback}>Reset</Options>}
 
-      <Options onClick={() => updateFeedback('good')}>Good</Options>
-      <Options onClick={() => updateFeedback('neutral')}>Neutral</Options>
-      <Options onClick={() => updateFeedback('bad')}>Bad</Options>
-      {!nullFeedback && <Options onBtnClick={resetFeedback}>Reset</Options>}
-
-      {totalFeedback > 0 ? 
-      <Feedback
-        noFeedback={nullFeedback}
-        good={goodFeedback}
-        bad={badFeedback}
-        neutral={neutralFeedback}
-        total={totalFeedback}
-        positive={positiveFeedback}
+        {totalFeedback > 0 ? 
+            <Feedback
+                noFeedback={nullFeedback}
+                good={good}
+                bad={bad}
+                neutral={neutral}
+                total={totalFeedback}
+                positive={positiveFeedback}
         />
-         : <Notification />}
+            : <Notification />
+        }
     </>
   );
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,42 +1,67 @@
 import css from "./Options.module.css";
 
 export default function Options({ like, reset }) {
-  function isGood() {
-    const feedBack = "good";
-    like(feedBack);
-  }
-  function isNeutral() {
-    const feedBack = "neutral";
-    like(feedBack);
-  }
-  function isBad() {
-    const feedBack = "bad";
-    like(feedBack);
-  }
-  function isReset() {
+  const handleClick = (feedback) => {
+    like(feedback);
+  };
+
+  const isReset = () => {
     reset();
-  }
+  };
+
   return (
     <ul className={css.list}>
       <li>
-        <button className={css.button} value={"good"} onClick={isGood}>
-          Good
-        </button>
+        <button className={css.button} value={"good"} onClick={() => handleClick("good")}>Good</button>
       </li>
       <li>
-        <button className={css.button} value={"neutral"} onClick={isNeutral}>
-          Neutral
-        </button>
+        <button className={css.button} value={"neutral"} onClick={() => handleClick("neutral")}>Neutral</button>
       </li>
       <li>
-        <button className={css.button} value={"bad"} onClick={isBad}>
-          Bad
-        </button>
+        <button className={css.button} value={"bad"} onClick={() => handleClick("bad")}>Bad</button>
       </li>
-
       <li>
         <button className={css.button} onClick={isReset}>Reset</button>
       </li>
     </ul>
   );
 }
+
+
+
+
+// import css from "./Options.module.css";
+
+// export default function Options({ like, reset }) {
+//   function isGood() {
+//     const feedBack = "good";
+//     like(feedBack);
+//   }
+//   function isNeutral() {
+//     const feedBack = "neutral";
+//     like(feedBack);
+//   }
+//   function isBad() {
+//     const feedBack = "bad";
+//     like(feedBack);
+//   }
+//   function isReset() {
+//     reset();
+//   }
+//   return (
+//     <ul className={css.list}>
+//       <li>
+//         <button className={css.button} value={"good"} onClick={isGood}>Good</button>
+//       </li>
+//       <li>
+//         <button className={css.button} value={"neutral"} onClick={isNeutral}>Neutral</button>
+//       </li>
+//       <li>
+//         <button className={css.button} value={"bad"} onClick={isBad}>Bad</button>
+//       </li>
+//       <li>
+//         <button className={css.button} onClick={isReset}>Reset</button>
+//       </li>
+//     </ul>
+//   );
+// }
